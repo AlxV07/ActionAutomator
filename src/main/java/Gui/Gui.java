8e61ca80e01b620140cbee55f9bdd9bbb2bbe5df
AlxV05 <alexander.kai.chen@gmail.com>
2023-05-08 17:13:42 -0500

    functioning basic gui!!!

--- src/main/java/Gui/Gui.java
+++ src/main/java/Gui/Gui.java
@@ -1,5 +1,11 @@
 package Gui;
 
+import ActionManagement.ActionBuilder;
+import ActionManagement.ActionSubTaskSequenceBuilder;
+import ActionManagement.ActionsHandler;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -7,21 +13,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Gui {
+    private final ActionsHandler actionsHandler = new ActionsHandler();
+    private final ActionBuilder builder = new ActionBuilder();
+
     private final JFrame f = new JFrame();
 
     private final DefaultListModel<String> model = new DefaultListModel<>();
     private final JList<String> actions = new JList<>(model);
     private final ActionsListUpdater actionsListUpdater = new ActionsListUpdater();
-    private final JButton buildNewAction = new JButton("+");
+    private final JButton buildNewAction = new TextPromptButton("+");
     private final JButton removeAction = new JButton("_");
     private final JLabel selectedAction = new JLabel("Default Text But Longer");
     private final JButton startAction = new JButton("Run:");
 
-    private final JButton setBuildEndKey = new JButton();
-    private final JLabel buildEndKey = new JLabel();
+    private final JLabel buildEndKey = new JLabel("None");
+    private final KeyButton setBuildEndKey = new KeyButton("Set Build End Key:", buildEndKey);
 
-    private final JButton setInterruptKey = new JButton();
-    private final JLabel interruptKey = new JLabel();
+    private final JLabel interruptKey = new JLabel("None");
+    private final JButton setInterruptKey = new KeyButton("Set Interrupt Key:", interruptKey);
 
 
     private void setUpFrame() {
@@ -55,9 +64,6 @@ public class Gui {
         actions.setBounds(25, 55, 130, 255);
         actions.setFont(new Font("Arial", Font.PLAIN, 12));
         actions.setFocusable(false);
-        for (int i = 0; i < 10; i++) {
-            model.addElement(String.valueOf(i));
-        }
         actions.addListSelectionListener(actionsListUpdater);
         f.add(actions);
     }
@@ -88,20 +94,101 @@ public class Gui {
         startAction.setMargin(insets);
         startAction.setFont(font);
         startAction.setFocusPainted(false);
+        startAction.addActionListener(e -> actionsHandler.executeAction(actions.getSelectedValue()));
         f.add(startAction);
+
+        setBuildEndKey.setBounds(170, 60, 80, 20);
+        setBuildEndKey.setMargin(insets);
+        setBuildEndKey.setFont(font);
+        f.add(setBuildEndKey);
+
+        setInterruptKey.setBounds(170, 90, 80, 20);
+        setInterruptKey.setMargin(insets);
+        setInterruptKey.setFont(font);
+        f.add(setInterruptKey);
     }
 
     private void setUpLabels() {
         Font font = new Font("Arial", Font.PLAIN, 10);
+
         selectedAction.setBounds(125, 25, 300, 20);
         selectedAction.setFont(font);
         f.add(selectedAction);
+
+        buildEndKey.setBounds(260, 60, 300, 20);
+        buildEndKey.setFont(font);
+        f.add(buildEndKey);
+
+        interruptKey.setBounds(260, 90, 300, 20);
+        interruptKey.setFont(font);
+        f.add(interruptKey);
     }
 
     private class ActionsListUpdater implements ListSelectionListener {
         @Override
         public void valueChanged(ListSelectionEvent e) {
             selectedAction.setText(actions.getSelectedValue());
+        }
+    }
+
+    private static class KeyButton extends JButton implements KeyListener {
+        private boolean waitingForKey = false;
+        public int keyPressed;
+        private final JLabel label;
+
+        public KeyButton(String text, JLabel label) {
+            super(text);
+            this.label = label;
+            addActionListener(e -> {
+                this.label.setText("Press a key...");
+                requestFocusInWindow();
+                waitingForKey = true;
+            });
+            addKeyListener(this);
+            setFocusPainted(false);
+        }
+
+        public void keyPressed(KeyEvent e) {
+            if (waitingForKey) {
+                int keyCode = e.getKeyCode();
+                if (keyCode != KeyEvent.VK_BACK_SPACE) {
+                    keyPressed = e.getKeyCode();
+                    label.setText(KeyEvent.getKeyText(keyCode));
+                } else {
+                    label.setText("None");
+                }
+                waitingForKey = false;
+            }
+        }
+
+        public void keyTyped(KeyEvent e) {
+        }
+
+        public void keyReleased(KeyEvent e) {
+        }
+    }
+
+    private class TextPromptButton extends JButton {
+
+        public TextPromptButton(String txt) {
+            super(txt);
+            addActionListener(e -> {
+                String name = JOptionPane.showInputDialog("Enter name:");
+                if (name != null && !name.isEmpty()) {
+                    try {
+                        GlobalScreen.registerNativeHook();
+                        if (!buildEndKey.getText().equals("None")) {
+                            actionsHandler.setAction(name, builder.buildAction(setBuildEndKey.keyPressed));
+                        } else {
+                            actionsHandler.setAction(name, builder.buildAction());
+                        }
+                        GlobalScreen.unregisterNativeHook();
+                        Gui.this.model.addElement(name);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
         }
     }
 
