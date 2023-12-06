3091fbb1143df330165fdebda7e93eefe22364cc
alxv05 <alexander.kai.chen@gmail.com>
2023-12-06 08:04:55 -0600

    removed todo

--- src/main/java/com/actionautomator/Main.java
+++ src/main/java/com/actionautomator/Main.java
@@ -5,11 +5,6 @@ import com.actionautomator.Gui.GuiMainFrame;
 import java.awt.*;
 
 public class Main {
-    /*
-    TODO:
-        - Code Documentation (README)
-        - Logo
-     */
     public static void main(String[] args) {
         GuiMainFrame guiMainFrame = new GuiMainFrame();
         guiMainFrame.start(true, Color.GREEN, Color.GRAY);
